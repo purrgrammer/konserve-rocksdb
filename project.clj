46a50064f3ea60fe6ee6c2ccfdafbbfd81dc2ba7
Alejandro Gomez <alejandro.gomez@47deg.com>
2019-08-30 23:52:43 +0200

    Change org name

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject io.replikativ/konserve-rocksdb "0.1.0"
+(defproject org.clojars.purrgrammer/konserve-rocksdb "0.1.0"
   :description "A RocksDB backend for konserve."
   :url "https://github.com/purrgrammer/konserve-rocksdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [io.replikativ/konserve "0.5.0"]
-                 [byte-streams "0.2.2"]
-;;                [org.rocksdb/rocksdbjni "5.5.1"]
-                 [kotyo/clj-rocksdb "0.1.3" :exclusions [byte-streams]]])
+                 [kotyo/clj-rocksdb "0.1.3"]])
 
